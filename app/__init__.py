d3e8f8794ff441385a5feb655cf653f5583088fe
man-mojyo <anonim.djoy@mail.ru>
2024-06-08 12:59:07 +0900

    2 commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-
+from app.database import db, migrate
 from app import views
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.add_url_rule("/", view_func=views.index_page)
 
